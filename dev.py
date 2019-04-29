bfdff1d97f1ac7a6693f65a432528f8cdcad49b3
juan <dopplerjuan@gmail.com>
2019-04-29 14:47:50 -0300

    Slider andando, queda por integrar al codigo

--- dev.py
+++ dev.py
@@ -6,25 +6,24 @@ Created on Mon Apr 29 13:58:09 2019
 """
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider
+import numpy as np
 import adquisicion
 
 
+#%%
 def set_trigger(dev, channels, variables=[], method='umbral', tmax=1, fs=44150):
     def update(val):
-    #    amp = samp.val
-    #    freq = sfreq.val
-    #    l.set_ydata(amp*np.sin(2*np.pi*freq*t))
+        l.set_ydata(val)
         fig.canvas.draw_idle()
-        print(l.get_ydata())
-        
     time, med = adquisicion.medicion_finita(dev=dev, channels=channels, tmax=tmax, fs=fs)
+    med = np.sin(time)
     fig, ax = plt.subplots()
     plt.subplots_adjust(bottom=0.25)
     ax.plot(time, med[0], lw=2, color='red')
     axcolor = 'lightgoldenrodyellow'
-    axumbral = plt.axes([0.25, 0.1, 0.65, 0.03], facecolor=axcolor)
+    axumbral = plt.axes([0.2, 0.1, 0.65, 0.03], facecolor=axcolor)
     umbral = 0
-    sumbral = Slider(axumbral, 'Umbral', 0.0, 5, valinit=umbral)
+    sumbral = Slider(axumbral, 'Umbral', 0.0, 1., valinit=umbral)
     l = ax.axhline(umbral)
     sumbral.on_changed(update)
     plt.show()
