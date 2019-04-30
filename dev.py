5c895486d4487e1dda6e759ee1a68adb12f5b929
jfdoppler <jdoppler@df.uba.ar>
2019-04-30 12:16:31 -0300

    Intento agregar calculo de integral con posibilidad de agregar nuevas funciones. En proceso (ver grafico, ubicacion sliders, separar figuras)

--- dev.py
+++ dev.py
@@ -9,9 +9,18 @@ from matplotlib.widgets import Slider
 import numpy as np
 import adquisicion
 import nidaqmx
+import pandas as pd
 
 #%%
-def set_trigger(dev, channels, tmed=1, fs=44150):
+def integral(data, fs, dt_integral):
+    window = int(fs*dt_integral)
+    series = pd.Series(data)
+    value = series.rolling(window=window, min_periods=1, center=True).sum()/fs
+    return value
+
+
+def set_trigger(dev, channels, tmed=1, fs=44150, funciones=[integral],
+                **kwargs):
     def create_update(axis, hline):
         def update(val):
             hline.set_ydata(val)
@@ -30,28 +39,36 @@ def set_trigger(dev, channels, tmed=1, fs=44150):
     time, med = adquisicion.medicion_finita(dev=dev, channels=channels,
                                             tmed=tmed, fs=fs)
     channel_names = list(channels.keys())
-    nplots = len(channels)
-    fig, ax = plt.subplots(nrows=1, ncols=nplots, figsize=(6*nplots, 4),
+    ncols = len(channels)
+    nrows = 1+len(funciones)
+    fig, ax = plt.subplots(nrows=nrows, ncols=ncols,
+                           figsize=(6*ncols, 3*nrows),
                            squeeze=False)
     plt.subplots_adjust(bottom=0.25)
     sliders = []
     updates = []
-    for nchannel in range(nplots):
+    for nchannel in range(ncols):
         ch_name = channel_names[nchannel]
         ncol = nchannel
-        ax[0, ncol].plot(time, med[ncol], lw=2, color='red',
-                      label='{}'.format(ch_name))
-        axumbral = plt.axes([ncol/nplots+0.1, 0.0, 1/nplots-0.15, 0.03])
-        umbral = np.mean(med[nchannel])
-        sliders.append(Slider(axumbral, 'Umbral {}'.format(ch_name),
-                              min((0.0, min(med[ncol]))),
-                              max((0.5, max(med[ncol]))),
-                              valinit=umbral))
-        hline = ax[0, ncol].axhline(umbral, ls='dashed')
-        update_func = create_update(axis=ax[0, ncol], hline=hline)
-        sliders[-1].on_changed(update_func)
-        updates.append(update_func)
-    fig.tight_layout()
+        for nrow in range(nrows):
+            if nrow == 0:
+                variable = med[ncol]
+            else:
+                variable = funciones[nrow-1](med[ncol], fs=fs, **kwargs)
+            ax[nrow, ncol].plot(time, variable, lw=2, color='red',
+                                label='{}'.format(ch_name))
+            axumbral = plt.axes([ncol/ncols+0.1, nrow/nrows, 1/ncols-0.15, 
+                                 0.03])
+            umbral = np.mean(med[ncol])
+            sliders.append(Slider(axumbral, 'Umbral {}'.format(ch_name),
+                                  min((0.0, min(variable))),
+                                  max((0.5, max(variable))),
+                                  valinit=umbral))
+            hline = ax[nrow, ncol].axhline(umbral, ls='dashed')
+            update_func = create_update(axis=ax[nrow, ncol], hline=hline)
+            sliders[-1].on_changed(update_func)
+            updates.append(update_func)
+#    fig.tight_layout()
     plt.show()
     return fig, ax, sliders, updates
 
@@ -59,4 +76,5 @@ channels = {'sound': 'ai0', 'vs': 'ai1'}
 fs = 44150
 tmed = 1
 dev = nidaqmx.system.device.Device('Dev1')
-fig, ax, sliders, updates = set_trigger(dev, channels)
+fig, ax, sliders, updates = set_trigger(dev, channels, tmed=tmed, fs=fs,
+                                        dt_integral=0.1)
