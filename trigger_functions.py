07d49038ebd7f15d40fc433a97b0d96c7ff2247f
juan <dopplerjuan@gmail.com>
2019-05-02 12:35:37 -0300

    Muevo funciones de trigger a archivo aparte

---
+++ trigger_functions.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  2 11:14:46 2019
+
+@author: juan
+"""
+import numpy as np
+import pandas as pd
+
+
+def integral(time, data, dt_integral):
+    fs = 1/(time[1]-time[0])
+    window = int(fs*dt_integral)
+    series = pd.Series(np.abs(data))
+    value = series.rolling(window=window, min_periods=1, center=True).sum()/fs
+    return value
+
+
+def modulo(time, data):
+    value = np.abs(data)
+    return value
